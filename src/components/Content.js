dc69b4ca76dada109afdcc584e8a141600c7f40e
hoangviet0100 <72763156+hoangviet0100@users.noreply.github.com>
2022-01-24 20:56:34 +0700

    Initial commit

---
+++ src/components/Content.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import '../assets/css/content.css'
+function Content() {
+  return <div className='content'>
+    <div className="row row-cols-1 row-cols-md-3 g-4 container1 ">
+      <div className='container'>
+        <div className="col col1">
+          <div className="card">
+            <img src="https://cdn.pixabay.com/photo/2012/04/02/17/45/input-25064_960_720.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title"></h5>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+        <div className="col col1">
+          <div className="card">
+            <img src="https://cdn.pixabay.com/photo/2012/04/02/17/45/input-25064_960_720.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title"></h5>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+        <div className="col col1">
+          <div className="card">
+            <img src="https://cdn.pixabay.com/photo/2012/04/02/17/45/input-25064_960_720.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title"></h5>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+        <div className="col col1">
+          <div className="card">
+            <img src="https://cdn.pixabay.com/photo/2012/04/02/17/45/input-25064_960_720.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title"></h5>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+        <div className="col col1">
+          <div className="card">
+            <img src="https://cdn.pixabay.com/photo/2012/04/02/17/45/input-25064_960_720.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title"></h5>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+        <div className="col col1">
+          <div className="card">
+            <img src="https://cdn.pixabay.com/photo/2012/04/02/17/45/input-25064_960_720.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title"></h5>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
+}
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../assets/css/footer.css'
+function Footer() {
+  return <div>
+      <footer className="page-footer font-small blue pt-4">
+    <div className="container-fluid text-center text-md-left">
+        <div className="row row1">
+            <div className="col-md-6 mt-md-0 mt-3 row-item">
+              <br/>
+                <h5 className="text-uppercase"><b>Hệ thống Quản lý văn bản & Hồ sơ công việc.</b></h5>
+                <p><b>Phát triển bởi Trung tâm CNTT, Trường Đại học Sư phạm Hà Nội.</b></p>
+            </div>
+
+            <hr className="clearfix w-100 d-md-none pb-0"/>
+
+            
+        </div>
+    </div>
+
+    
+
+</footer>
+  </div>;
+}
+
+export default Footer;
